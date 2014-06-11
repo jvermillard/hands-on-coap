4446f08db20ad75eb9e4f5016051f8c9234d459c
Julien Vermillard <jvermillard@sierrawireless.com>
2014-06-11 16:25:04 +0200

    step3 to complete

---
+++ step3/src/step3/HelloWorldClient.java
@@ -0,0 +1,12 @@
+package step3;
+
+import ch.ethz.inf.vs.californium.coap.Request;
+
+public class HelloWorldClient {
+
+    public static void main(String[] args) {
+        Request request = Request.newGet();
+        // TOOD :)
+    }
+
+}
